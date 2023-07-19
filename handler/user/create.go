6f8a02743f4d0888d7f3d9da8779b90a202c74f2
xlearns <2447684095@qq.com>
2023-07-19 17:58:24 +0800

    feat: add user interface

--- handler/user/create.go
+++ handler/user/create.go
@@ -1,42 +1,41 @@
-package user
+package router
 
 import (
-	"fmt"
 	"net/http"
 
-	"apiserver/pkg/errno"
+	"apiserver/handler/sd"
+	"apiserver/handler/user"
+	"apiserver/router/middleware"
 
 	"github.com/gin-gonic/gin"
-	"github.com/lexkong/log"
 )
 
-// Create creates a new user account.
-func Create(c *gin.Context) {
-	var r struct {
-		Username string `json:"username"`
-		Password string `json:"password"`
+// Load loads the middlewares, routes, handlers.
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// Middlewares.
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	// 404 Handler.
+	g.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "The incorrect API route.")
+	})
+
+	u := g.Group("/v1/user")
+	{
+		u.POST("", user.Create)
 	}
 
-	var err error
-	if err := c.Bind(&r); err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": errno.ErrBind})
-		return
+	// The health check handlers
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
 	}
 
-	log.Debugf("username is: [%s], password is [%s]", r.Username, r.Password)
-	if r.Username == "" {
-		err = errno.New(errno.ErrUserNotFound, fmt.Errorf("username can not found in db: xx.xx.xx.xx")).Add("This is add message.")
-		log.Errorf(err, "Get an error")
-	}
-
-	if errno.IsErrUserNotFound(err) {
-		log.Debug("err type is ErrUserNotFound")
-	}
-
-	if r.Password == "" {
-		err = fmt.Errorf("password is empty")
-	}
-
-	code, message := errno.DecodeErr(err)
-	c.JSON(http.StatusOK, gin.H{"code": code, "message": message})
+	return g
 }
